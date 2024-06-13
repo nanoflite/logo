b4b69eaa15f4a4fc686aebb119cc7160ea0ea555
Johan Van den Brande <johan@vandenbrande.com>
2024-06-13 20:33:25 +0200

    Fix logo

--- logo.js
+++ logo.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const path = require('path')
 
 const parser = require('./logo_parser').parser
-const interpret = require('./interpret2')
-const turtle = require("./turtle2")()
+const interpret = require('./logo_interpreter')
+const turtle = require("./logo_turtle")()
 
 const name = process.argv[2]
 const script = fs.readFileSync(name, { encoding: 'utf8' })
